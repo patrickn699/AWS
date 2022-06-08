078f8ea796975c228210870e2ba3b77ec4a4dc55
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2022-06-08 13:09:47 +0530

    added: new script

--- ec2-sns/auto.sh
+++ ec2-sns/auto.sh
@@ -3,6 +3,5 @@ sudo yum install git -y
 git clone https://github.com/patrickn699/AWS.git
 cd AWS
 cd ec2-sns
-pip3 install streamlit
-pip3 install boto3
-streamlit run main.py
+sudo chmod u+x semi-auto.sh
+sudo ./semi-auto.sh
---
+++ ec2-sns/semi-auto.sh
@@ -0,0 +1,3 @@
+pip3 install streamlit
+pip3 install boto3
+streamlit run main.py
